fba16f41646a76166138214d0c7d0e6c79636307
OckHYEWON <okewon@naver.com>
2020-03-31 17:17:58 +0900

    Thread 학습을 위한 단일 쓰레드 개념 학습 및 쓰레드 예제 실습

---
+++ ThreadTest/src/kr/or/ddit/basic/T01_ThreadTest.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.basic;
+
+public class T01_ThreadTest {
+
+	public static void main(String[] args) {
+		//싱글 쓰레드 프로그램
+		for(int i = 1; i <= 200; i++) {
+			System.out.println("*");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i <= 200; i++) {
+			System.out.println("$");
+		}
+	}
+	
+}
